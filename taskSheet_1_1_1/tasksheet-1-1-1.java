a8c61b6fa2ea7d1542da3575b9afe49906ad523e
stan-asian <stanleypanag18@gmail.com>
2025-05-03 03:34:37 +0000

    Update task 1-1-1

--- taskSheet_1_1_1/tasksheet-1-1-1.java
+++ taskSheet_1_1_1/tasksheet-1-1-1.java
@@ -2,6 +2,6 @@ package taskSheet_1_1_1;
 
 class helloWorldApp{
     public static void main(String[] args) {
-        System.out.println("Kamusta World!"); // Display the string.
+        System.out.println("Kamusta Mundo!"); // Display the string.
     }
 }
