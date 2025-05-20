1d11eab4f755d9b9cfc87a88ffd4bafb33ffa2a3
stan-asian <stanleypanag18@gmail.com>
2025-05-20 06:16:55 +0000

    TASKSHEET 12 COMPLETED

---
+++ taskSheet_12/Scale.java
@@ -0,0 +1,45 @@
+package taskSheet_12;
+
+//INTERFACE
+interface PointMethods{
+    abstract void scale();
+    abstract void print();
+}
+
+//PSVM
+public class Scale {
+
+    public static void main(String[] args) {
+
+        Point p = new Point(32,32);
+        int step = 0;
+        while(step < 10){
+            p.scale();
+            p.print();
+            step++;
+        }
+    }
+}
+
+// SUB-CLASS
+class Point implements PointMethods{
+    private double x, y;
+
+    // Constructor
+    Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void scale(){
+        this.x = this.x / 2;
+        this.y = this.y / 2;
+    }
+
+    @Override
+    public void print(){
+        System.out.println("FINAL RESULT: " + "(" + x + ", " + y + ")");
+    }
+
+}
