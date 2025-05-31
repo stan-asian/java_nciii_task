49e26f106da93d840105714c962710c1bce63b0b
stan-asian <stanleypanag18@gmail.com>
2025-06-01 01:59:56 +0800

    Tasksheet 2 commit

---
+++ taskSheet_2/taskSheet2.java
@@ -0,0 +1,14 @@
+package taskSheet_2;
+
+public class taskSheet2 {
+    public static void main(String[] args) {
+        char a = 'H';
+        char b = 'w';
+        int c = 3110;
+        byte d = 0;
+        String e = "rld";
+        String f = "2.0";
+        boolean g = true;
+        System.out.println(a + String.valueOf(c) + " " + b + String.valueOf(d) + e + " " + f + " " + g);
+    }
+}
