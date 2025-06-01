f27c5b29f9b971240fc770a1efab7d79b0a451de
stan-asian <stanleypanag18@gmail.com>
2025-06-01 04:58:46 +0000

    taskSheet 14 Done!

---
+++ taskSheet_14/taskSheet14.java
@@ -0,0 +1,33 @@
+package taskSheet_14;
+
+public class taskSheet14 {
+    public static void main(String[] args) {
+        A a = new A();
+        a.abstractMethod();
+        System.out.println(a.sayHello("Maria"));
+
+        B b = new B();
+        b.abstractMethod();
+        System.out.println(b.sayHello("Proline"));
+    }
+}
+
+abstract class abstractClass{
+    abstract void abstractMethod();
+    String sayHello(String name){
+        return "Hello " + name;
+    }
+}
+
+class A extends abstractClass{
+    @Override
+    void abstractMethod(){
+        System.out.println("A's implementation of abstract method!");
+    }
+}
+class B extends abstractClass{
+    @Override
+    void abstractMethod(){
+        System.out.println("B's implementation of abstract method!");
+    }
+}
